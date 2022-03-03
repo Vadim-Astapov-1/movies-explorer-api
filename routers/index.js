89520d6a97d5d81239e7b6493ad047da6af1b977
Vadim Astapov <mr.chibichan@yandex.ru>
2022-03-03 17:56:13 +0100

    actual version

--- routers/index.js
+++ routers/index.js
@@ -9,12 +9,6 @@ const { auth } = require('../middlewares/auth');
 const logout = require('../middlewares/logout');
 const { validateCreateUser, validateLogin } = require('../middlewares/validations');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.post('/signin', validateLogin, login);
 router.post('/signup', validateCreateUser, createUser);
 
