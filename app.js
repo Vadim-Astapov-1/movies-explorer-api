dde6a446c27f08e1f0159dfcf04b8f2753b95f84
Vadim Astapov <mr.chibichan@yandex.ru>
2022-03-03 17:44:40 +0100

    load: crash-test

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.use(requestLogger);
 
 app.use(cors);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 
 app.use(errorLogger);
